68a4cb2008906dbd859b769ddfb213e802f1f613
inclinedsujal <89937345+inclinedsujal@users.noreply.github.com>
2024-11-24 11:23:18 +0000

    datatypes

---
+++ local/datatype.js
@@ -0,0 +1,19 @@
+
+"use strict";// treat all js code AS new version//
+
+//alert("hwello")
+
+console.log(3+3); 
+
+
+//Boolean=> true/false
+//null=>standalone value
+
+//undefined=>value abhi assign nhi hui hi
+
+//symbol => unique
+
+//object
+console.log(typeof"sujal");
+
+console.log(typeof undefined);
