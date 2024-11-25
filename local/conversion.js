72c4eb30848439083e98115ab8b538968979df73
inclinedsujal <89937345+inclinedsujal@users.noreply.github.com>
2024-11-25 12:00:27 +0000

    conversion of datatypes

---
+++ local/conversion.js
@@ -0,0 +1,32 @@
+let score="33"
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+
+let valueInNumber=Number(score);
+
+console.log(typeof(valueInNumber));
+
+let scores=null;
+let scoree=undefined;
+
+console.log(typeof (scoree));
+
+console.log(typeof scores);
+
+
+// "33"=>33
+//"33abc"=>Nan;
+
+//true=>1; false => 0;
+
+//""=>false
+//"sujal"=>true
+
+let somenumber=33
+
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
