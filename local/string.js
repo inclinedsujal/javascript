245217d8c3a75f8d993b48f03795ac46b485c88e
inclinedsujal <89937345+inclinedsujal@users.noreply.github.com>
2024-11-26 18:28:33 +0000

    string

---
+++ local/string.js
@@ -0,0 +1,57 @@
+
+//types of memory
+// stack (primitive)heap memory(non primitive)
+
+//in stack value can be copy
+///in heap value assign original
+
+/*
+let mymail="sujalsuneja.com"
+
+let anothermail=mymail
+
+anothermail="sujalsuneja12344.com"
+
+console.log(mymail);
+
+console.log(anothermail)
+
+
+let user={
+    email:"gmail.com",
+    phn:"mobile",
+
+}
+
+let user2=user
+
+user2.email="yahoo.com"
+
+console.log(user.email);
+console.log(user2.email);
+*/
+
+
+const name="sujal"
+
+const repcount=40;
+
+
+
+
+//console.log(`hello my name is${name)} and my repcountis ${repcount}`)
+
+const game=new String('sujalss')
+
+console.log(game[2])
+
+console.log(game.__proto__);
+
+console.log(game.toUpperCase());
+console.log(game.length);
+
+console.log(game.charAt(2));
+
+
+
+
