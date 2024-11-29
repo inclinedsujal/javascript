efb3daf50b1143277bc402b79bf724fe88d2b74c
inclinedsujal <89937345+inclinedsujal@users.noreply.github.com>
2024-11-29 10:48:13 +0000

    array

---
+++ local/array.js
@@ -0,0 +1,140 @@
+//array///
+
+const myarr=[0,1,3,4,5,567,7,7,"sujal"]
+
+const myarr2=["sujal","sejal","sajal"]
+
+    console.log(myarr[1]);
+    console.log(myarr2[2]);
+
+    //array methods
+
+    myarr.push(23)
+    myarr.push(234)
+    myarr.pop()
+
+    
+    myarr.unshift(9)
+    myarr.shift()
+    console.log(myarr.includes(8))
+
+    console.log(myarr)
+
+
+// slice splice 
+
+console.log("A",myarr);
+
+
+const arr=[1,2,3,4,45,345]
+
+console.log(arr.slice(1,5))
+
+console.log(arr.splice(1,5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
