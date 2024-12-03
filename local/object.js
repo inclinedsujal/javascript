975a5ed0cd0608a9c8ddf45ec48faa281f74497f
inclinedsujal <89937345+inclinedsujal@users.noreply.github.com>
2024-12-03 11:18:49 +0000

    function

---
+++ local/object.js
@@ -0,0 +1,17 @@
+//singleton//
+
+//object literals//
+
+
+const user={
+
+    name:"sujal",
+    age:19,
+    location:"Rtk",
+    loggedin:false,
+    lastlogin:["mon","tues","wed","fri"]
+
+}
+
+console.log(user.age)
+console.log(user["email"])
